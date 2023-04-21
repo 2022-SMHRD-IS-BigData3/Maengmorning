010a5a5103229cd8ca7a745f63a944322de7e51d
smhrd <smhrd@DESKTOP-IB1VH5J>
2023-04-21 16:29:09 +0900

    0421lswcommit

--- abcd/src/main/java/kr/mang/controller/ProductDetail.java
+++ abcd/src/main/java/kr/mang/controller/ProductDetail.java
@@ -4,11 +4,17 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kr.mang.model.ProductlistDAO;
+import kr.mang.model.ProductlistVO;
 public class ProductDetail implements Command{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int pro_id = Integer.parseInt(request.getParameter("pro_id"));
+		ProductlistDAO dao = new ProductlistDAO();
+		
+		ProductlistVO vo = dao.detail(pro_id);
 		
 		
-		return null;
+		return "상품상세정보.jsp";
 	}
 }
